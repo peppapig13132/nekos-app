898f189cb1a8467a2eaccc8d22c66a5dd3135550
peppapig13132 <peppapig13132@outlook.com>
2024-06-23 02:43:29 +0200

    Initial commit :tada:

---
+++ README.md
@@ -0,0 +1,6 @@
+# Nekos App
+
+![Node.js](https://img.shields.io/badge/node.js-20.12.2-blue)
+![npm](https://img.shields.io/badge/npm-10.6.0-blue)
+[![Tailwind CSS](https://img.shields.io/badge/tailwind_css-3.4.3-blue)](https://tailwindcss.com/)
+[![nekos.best](https://img.shields.io/badge/nekos.best-2.0-blue)](https://nekos.best/)
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h2 className="text-4xl my-3 text-slate-700">
+        Hello React.js!
+      </h2>
+      <p className="text-slate-700">
+        React.js boilerplate pre-configured with TypeScript and Tailwind CSS
+      </p>
+      <p className="mt-12 text-slate-700">Made with ❤️ by <a target="_blank" href="https://github.com/peppapig13132">Pepp</a></p>
+    </div>
+  );
+}
+
+export default App;

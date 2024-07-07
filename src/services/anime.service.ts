cdc6439efab01fc7da63042b4bca235acb5e98aa
peppapig13132 <peppapig13132@outlook.com>
2024-07-07 14:54:51 +0200

    Wrap api call in try-catch

--- src/services/anime.service.ts
+++ src/services/anime.service.ts
@@ -1,6 +1,22 @@
 import { AnimeResponse } from '../interfaces/service.interface';
 
+const defaultAnimeResponse: AnimeResponse = {
+  results: []
+}
+
 export const getAnime = async (category: string, count: number): Promise<AnimeResponse> => {
-  const response = await fetch(`https://nekos.best/api/v2/${category}?amount=${count}`);
-  return await response.json();
+  try {
+    const response = await fetch(`https://nekos.best/api/v2/${category}?amount=${count}`);
+    
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+    
+    const data: AnimeResponse = await response.json();
+    
+    return data;
+  } catch (e) {
+    console.error('Fetching anime data failed:', e);
+    return defaultAnimeResponse;
+  }
 }
